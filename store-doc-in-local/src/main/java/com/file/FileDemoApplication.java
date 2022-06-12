bd26db07231779f158a6843c8a6e0f940bc338ab
ambuj018 <mr.ambujkr@gmail.com>
2022-06-12 20:08:07 +0530

    3rd commit - ambuj

--- store-doc-in-local/src/main/java/com/file/FileDemoApplication.java
+++ store-doc-in-local/src/main/java/com/file/FileDemoApplication.java
@@ -9,6 +9,8 @@ public class FileDemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FileDemoApplication.class, args);
 		
+		// don't to anything here
+		// do coding only in another package level class
 	}
 }
 
